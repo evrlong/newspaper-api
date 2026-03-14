bbba35ef3fd97ba8800e445b74723775694159c1
evrlong <127796398+evrlong@users.noreply.github.com>
2026-03-14 13:28:08 +0100

    Remove dist from repository

--- dist/routes/users.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { createUser, getUserByEmailHandler } from '../controllers/users.controller';
-const usersRouter = Router();
-usersRouter.post('/', createUser);
-usersRouter.get('/:email', getUserByEmailHandler);
-export default usersRouter;
